fc1213f6cd75f26c14d0c849dcba37ba1ba653cf
jlam609 <jlam609@gmail.com>
2021-04-27 19:26:31 -0400

    test rest api using flask

---
+++ api/api.py
@@ -0,0 +1,41 @@
+import flask
+import json
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+books = [
+    {'id': 0,
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'published': '1992'},
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'published': '1973'},
+    {'id': 2,
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'first_sentence': 'to wound the autumnal city.',
+     'published': '1975'}
+]
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return """
+    <h1>Test</h1>
+    <p>Test API</p>
+    """
+
+
+@app.route('/data', methods=['GET'])
+def data():
+    return (
+        flask.jsonify(books)
+    )
+
+
+app.run()
