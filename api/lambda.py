c2463ba23d5dba42caa7295928854aa51e3d0e97
jlam609 <jlam609@gmail.com>
2021-04-29 13:01:21 -0400

    sample lambda function

---
+++ api/lambda.py
@@ -0,0 +1,26 @@
+import json
+
+
+def lambdahandler(event, context):
+    # 1 parse out query strings
+    transactionId = event['queryStringParameters']['transactionId']
+    transactionType = event['queryStringParameters']['type']
+    transactionAmount = event['queryStringParameters']['amount']
+
+    print(transactionId, transactionType, transactionAmount)
+    # 2 construct body of response object
+    transactionResponse = {}
+    transactionResponse['transactionId'] = transactionId
+    transactionResponse['transactionType'] = transactionType
+    transactionResponse['transactionAmount'] = transactionAmount
+    transactionResponse['message'] = 'Hello World'
+
+    # 3 construct http response object
+    responseObject = {}
+    responseObject['statusCode'] = 200
+    responseObject['headers'] = {}
+    responseObject['headers']['Content-Type'] = 'application/json'
+    responseObject['body'] = json.dumps(transactionResponse)
+    
+    # 4 return response
+    return responseObject
